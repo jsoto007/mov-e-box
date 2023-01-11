5e29bb8acbb528d9a0d5c5e424e6292d42178077
Jose Soto <jsoto7087@gmail.com>
2023-01-11 01:05:18 -0500

    updated CSS

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,19 +4,24 @@ import { NavLink } from "react-router-dom";
 function MovieCard( { movieData } ) {
     const { id,title, year, genre, runtime, poster} = movieData
 
-    // function handleDeleteClick() {
-        
-    // }
+   function handleMouseOn() {
+      return (
+         <ul>
+            <li>Release: {year}</li>
+            <li>Genre: {genre}</li>
+            <li>Runtime: {runtime}</li>
+        </ul>
+      )
+   }
     return (
             <div className="movie-card">
-              
+                    <li>{title}</li>
                 <img src={poster} alt={title} />
-                <ul id="">
-                    <li>Release: {year}</li>
-                    <li>Genre: {genre}</li>
-                    <li>Runtime: {runtime}</li>
-                </ul>
-                <NavLink to={`/movies/${id}`}>
+                <br />
+                <NavLink 
+                  id="details"
+                  to={`/movies/${id}`}
+                >
                 Movie Details
                 </NavLink>
                 {/* <button id="delete-btn" onClick={handleDeleteClick}>🗑️</button> */}
